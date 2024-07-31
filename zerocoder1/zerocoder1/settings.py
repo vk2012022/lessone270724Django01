09a394ace143eac4f3bca2d584bec50af24e29d1
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-07-31 21:58:42 +0300

    add new code14

--- zerocoder1/zerocoder1/settings.py
+++ zerocoder1/zerocoder1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'news',
     'main',
     'myapp',
     'django.contrib.admin',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- zerocoder1/zerocoder1/urls.py
+++ zerocoder1/zerocoder1/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('myapp/', include('myapp.urls')),
+    path('news/', include('news.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
