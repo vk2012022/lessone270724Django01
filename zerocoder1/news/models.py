8d89001961297ed9cc18f996e07ae7fa3317624d
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-08-01 11:26:23 +0300

    add new code 010824

--- zerocoder1/news/models.py
+++ zerocoder1/news/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Создайте свои модели здесь.
 
 class News_post(models.Model):
     title = models.CharField('Название новости', max_length=50)
     short_description = models.CharField('Краткое описание новости', max_length=200)
     text = models.TextField('Новость')
     pub_date = models.DateTimeField('Дата публикации')
+    username = models.CharField('Имя пользователя', max_length=100)  # Новое поле для имени пользователя
 
     def __str__(self):
         return self.title
+
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
